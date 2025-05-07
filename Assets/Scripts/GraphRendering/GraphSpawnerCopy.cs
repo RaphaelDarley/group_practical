5d660509fe210a6ea40419916b1e6a8e5c174838
Zohaib Bhatti <93671428+ZohaibBhatti05@users.noreply.github.com>
2025-05-07 18:53:19 +0100

    switch to OVR, merge graph branch

---
+++ Assets/Scripts/GraphRendering/GraphSpawnerCopy.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class GraphSpawnerCopy : MonoBehaviour
+{
+    // CSV file to read, treating project directory as root folder
+    // Can safely reorder columns but must contain "stablecoin" "high" and "low" headers
+    // ! Currently assumes consistent initial timestamp and interval for each coin
+    // ! Data for each coin must be in a continuous block
+    public string Filepath = "Assets/Data/Stablecoins_dataset.csv";
+
+    // Material for the graph
+    public Material graphMat;
+
+    // Prefabs for positive and negative event markers
+    public GameObject PosPrefab;
+    public GameObject NegPrefab;
+
+    // Minimum line width to ensure it's actually visible even if it would have zero width
+    public float MinLineThickness = 0.05f;
+
+    // Height scaling (height of value=1)
+    public float GlobalHeightScale = 1;
+
+    // Array of relative heights for each coin, will be multiplied by global height
+    // Useful if coins of different orders of magnitude need to be compared
+    // Extra scales of 1 will be used if more coins than entries
+    // ! Must contain at least one entry
+    public float[] RelativeHeightScale = {1};
+
+    // Height of 0 value, used to translate all data up or down for easier view
+    public float HeightZero = 0;
+
+    // Width between data for different coins
+    public float CoinGap = 5;
+
+    // Horizontal gap between data entries
+    public float TimeEntryWidth = 0.2f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Read CSV file
+        string[] lines = File.ReadAllLines(Filepath);
+
+        // Get indices of relevant column headers
+        string[] headers = lines[0].Split(',');
+        int timestamp = Array.IndexOf(headers, "timestamp");
+        int high = Array.IndexOf(headers, "high");
+        int low = Array.IndexOf(headers, "low");
+        int stablecoin = Array.IndexOf(headers, "stablecoin");
+        int event_result = Array.IndexOf(headers, "event_result");
+
+        // Get dimensions of csv array, excluding header row
+        int length = lines.Length-1;
+        int width = headers.Length;
+
+        // Split each row into entries, excluding header row
+        List<string[]> rows = new List<string[]>();
+        for (int i=1; i<=length; i++)
+        {
+            rows.Add(lines[i].Split(','));
+        }
+
+        // Generate vertices, triangles and event markers from rows
+        Vector3[] vertices = new Vector3[length*2]; //Coordinates of high and low values for each timestamp and coin
+        int[] triangles = new int[length*12];       //Joining high and low points of adjacent timestamps with quadrilaterals. Will have a few trailing zeros left at the end, but just creates extra triangles with zero area so won't render
+        List<GameObject> events = new List<GameObject>();   //Store references to event marker objects
+        string currentcoin = rows[0][stablecoin];   //Name of current coin so we can see when it's changed
+        float timepos = 0;                          //x coordinate, equals (current_timestamp - initial_timestamp)*TimeEntryWidth/400
+        int coinnum = 0;                            //Counts how many different coins we've done, used for fetching relative scaling data
+        float coinpos = 0;                          //z coordinate. equals coinnum*CoinGap (storing separately to avoid repeat calculation)
+        float heightscale = GlobalHeightScale*RelativeHeightScale[coinnum]; //Height multiplier for current coin. By default RelativeHeightScale will contain at least one entry so shouldn't throw an error
+        // Iterate through rows
+        for (int i = 0; i < length; i++) 
+        {
+            string[] row = rows[i];
+        // Reset timepos and move to next coinpos if coin has changed
+            if (row[stablecoin]!=currentcoin)
+            {
+                coinnum ++;
+                coinpos = coinpos+CoinGap;
+                timepos = 0;
+                currentcoin = row[stablecoin];
+                try
+                {
+                    heightscale = GlobalHeightScale*RelativeHeightScale[coinnum];
+                }
+                catch
+                {
+                    heightscale = GlobalHeightScale;
+                }
+            }
+        // Read and scale high and low values for time interval
+            float h = float.Parse(row[high])*heightscale+HeightZero;
+            float l = float.Parse(row[low])*heightscale+HeightZero;
+            if (h==l) 
+            {
+                l = h-MinLineThickness;
+            }
+        // Create vertices for high and low point
+            vertices[2*i] = new Vector3(timepos,h,coinpos);
+            vertices[2*i+1] = new Vector3(timepos,l,coinpos);
+        // If not the start of a new coin, connect to previous vertices with triangles
+            if (timepos>0)
+            {
+                triangles[12*i] = 2*i-2;         // Can prob improve with iteration
+                triangles[12*i+1] = 2*i-1;
+                triangles[12*i+2] = 2*i;
+                triangles[12*i+3] = 2*i;
+                triangles[12*i+4] = 2*i-1;
+                triangles[12*i+5] = 2*i-2;
+                triangles[12*i+6] = 2*i-1;     
+                triangles[12*i+7] = 2*i;
+                triangles[12*i+8] = 2*i+1;
+                triangles[12*i+9] = 2*i+1;
+                triangles[12*i+10] = 2*i;
+                triangles[12*i+11] = 2*i-1;
+            }
+        // If event at this timestamp, add marker
+            if (row[event_result]=="positive")
+            {
+                events.Add(Instantiate(
+                    PosPrefab,
+                    vertices[2*i],          //Place marker centred at top of line. May change later
+                    Quaternion.identity,
+                    transform
+                ));
+            }
+            if (row[event_result]=="negative")
+            {
+                events.Add(Instantiate(
+                    NegPrefab,
+                    vertices[2*i],
+                    Quaternion.identity,
+                    transform
+                ));
+            }
+        // Move x position
+            timepos = timepos+TimeEntryWidth;
+        }
+
+
+        foreach (Vector3 vertex in vertices) {print(vertex);}
+
+        // Render mesh
+        var meshFilter = gameObject.AddComponent<MeshFilter>();
+        var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        meshFilter.sharedMesh = mesh;
+        meshRenderer.material = graphMat;
+    }
+}
---
+++ Assets/Scripts/Input/OVRControls.cs
@@ -0,0 +1,251 @@
+using Unity.XR.CoreUtils;
+using UnityEditor;
+using UnityEngine;
+
+public class OVRControls : MonoBehaviour
+{
+
+    [Header("User Objects")]
+    public Transform vrUser;
+    public Transform vrCamera;
+
+    [Header("Objects")]
+    public GameObject vrMenu;
+    public Transform vrMenuPosition;
+
+    [Header("Settings")]
+    public float moveSpeed;
+    public float turnSpeed;
+    public bool forceUniaxialTurning;
+    public bool disablePitch;
+    public bool allowStrafe;
+    public float zoomDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isZooming)
+        {
+            MoveUser();
+        }
+
+        RotateCamera();
+        UpdateZoom();
+
+        UpdateMenu();
+    }
+
+    // ============================== MOVEMENT ====================================================
+
+
+    // Move user relative to the plane spanned by the camera view
+    void MoveUser()
+    {
+        Vector2 vector = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        Vector3 move = moveSpeed * vector.y * 0.05f * vrCamera.forward;
+
+        if (allowStrafe)
+        {
+            move += moveSpeed * vector.x * 0.05f * vrCamera.right;
+        }
+
+        // move += UpdateElevation() * 0.05f;
+
+        vrUser.position += move;
+
+    }
+
+
+    // Elevate user relative to the global view
+    Vector3 UpdateElevation()
+    {
+        if (OVRInput.Get(OVRInput.Button.One))
+        {
+            return -moveSpeed * Vector3.up;
+        }
+        else if (OVRInput.Get(OVRInput.Button.Two))
+        {
+            return moveSpeed * Vector3.up;
+        }
+        return Vector3.zero;
+    }
+
+
+
+
+    // keep track of player pitch for pitch clamping
+    private float pitch = 0.0f;
+    public float maxPitch = 20.0f;
+
+    // rotate xr rig globally (not with respect to the camera to prevent rolling)
+    void RotateCamera()
+    {
+        // potentially reset camera
+        //if (OVRInput.Get(OVRInput.Button.Three, OVRInput.Controller.RTouch))
+        //    {
+        //    vrUser.rotation = Quaternion.identity;
+        //    pitch = 0.0f;
+        //}
+
+        Vector2 vector = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+
+        if (forceUniaxialTurning)
+        {
+            // change yaw globally
+            if (Mathf.Abs(vector.x) >= Mathf.Abs(vector.y))
+            {
+                float deltaYaw = vector.x * turnSpeed;
+                vrUser.Rotate(0f, deltaYaw, 0f, Space.World);
+            }
+            // change pitch locally
+            else if (!disablePitch)
+            {
+                float deltaPitch = -vector.y * turnSpeed;
+                deltaPitch = Mathf.Clamp(deltaPitch, -pitch - maxPitch, -pitch + maxPitch);
+                pitch += deltaPitch;
+                vrUser.Rotate(deltaPitch, 0f, 0f, Space.Self);
+            }
+        }
+        else
+        {
+            float deltaYaw = vector.x * turnSpeed;
+            float deltaPitch = -vector.y * turnSpeed;
+            deltaPitch = Mathf.Clamp(deltaPitch, -pitch - 45f, -pitch + 45f);
+            pitch += deltaPitch;
+
+            // change yaw globally
+            vrUser.Rotate(0f, deltaYaw, 0f, Space.World);
+
+            if (!disablePitch)
+            {
+                // change pitch locally
+                vrUser.Rotate(deltaPitch, 0f, 0f, Space.Self);
+            }
+
+        }
+    }
+
+
+
+    private Vector3 originalCameraPosition = Vector3.zero;
+    private Vector3 zoomDirection = Vector3.zero;
+    private bool isZooming = false;
+    private float zoomCoef = 0f;
+    private float zoomSpeed = 0.06f;
+    // project camera forwards (a crude zoom effect)
+    void UpdateZoom()
+    {
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger) > 0.5f)
+        {
+            if (!isZooming)
+            {
+                zoomDirection = vrCamera.forward;
+                originalCameraPosition = vrUser.position;
+                isZooming = true;
+            }
+            zoomCoef = Mathf.Clamp01(zoomCoef + zoomSpeed);
+            UpdateCameraZoom(true);
+
+        }
+        else
+        {
+            zoomCoef = Mathf.Clamp01(zoomCoef - zoomSpeed);
+            if (zoomCoef <= 0f && isZooming)
+            {
+                vrUser.position = originalCameraPosition;
+                isZooming = false;
+            }
+            if (isZooming)
+            {
+                UpdateCameraZoom(false);
+            }
+        }
+
+
+    }
+
+    // calculate new zoom target
+    private void UpdateCameraZoom(bool zoomingForward)
+    {
+        // update position
+        Vector3 forward = zoomDirection;
+        Vector3 zoomTargetPosition = originalCameraPosition + forward * zoomDistance;
+
+        if (zoomingForward)
+        {
+            Vector3 zoomPosition = Vector3.Slerp(originalCameraPosition, zoomTargetPosition, EaseOutExpo(zoomCoef));
+            vrUser.position = zoomPosition;
+        }
+        else
+        {
+            Vector3 zoomPosition = Vector3.Slerp(zoomTargetPosition, originalCameraPosition, EaseOutExpo(1 - zoomCoef));
+            vrUser.position = zoomPosition;
+        }
+    }
+
+    // helper function to smooth zooming
+    private float EaseOutExpo(float f)
+    {
+        if (f >= 1f)
+        {
+            return 1;
+        }
+        else
+        {
+            return 1 - Mathf.Pow(2, -10 * f);
+        }
+    }
+
+
+    // ====================================== UI INTERACTIONS =======================================================
+
+
+    // keep track of whether the menu is showing
+    private bool menuShowing = true;
+
+    // move menu to player position and toggle on menu button press
+    void UpdateMenu()
+    {
+        // move menu to player position
+        vrMenuPosition.SetPositionAndRotation(vrUser.position, vrUser.rotation);
+
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            menuShowing = !menuShowing;
+
+            vrMenu.SetActive(menuShowing);
+        }
+    }
+
+    public void ChangeMovementSpeed(float speed)
+    {
+        moveSpeed = speed;
+    }
+
+    public void ChangeTurnSpeed(float speed)
+    {
+        turnSpeed = speed;
+    }
+
+    public void ToggleUniaxialTurning(bool turning)
+    {
+        forceUniaxialTurning = !forceUniaxialTurning;
+    }
+
+    public void ToggleStrafe(bool strafe)
+    {
+        allowStrafe = !allowStrafe;
+    }
+
+    public void ChangeZoomDistance(float distance)
+    {
+        zoomDistance = distance;
+    }
+}
